705c96af262d96ea68c5eefef931daa94e5ba30d
Zhu Hailiang <imyaxi@126.com>
2020-03-18 16:02:37 +0800

    Initial Commit

---
+++ ios-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-test
+//
+//  Created by 朱海亮 on 2020/3/18.
+//  Copyright © 2020 朱海亮. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
